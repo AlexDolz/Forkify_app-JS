88384cede13dfc73c2df7fb9a628fe026134b928
AlexDolz <aleksejdolznikov3@gmail.com>
2024-02-27 15:54:50 +0100

    listening for load and haschange events

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,7 +2,6 @@
 import icons from 'url:../img/icons.svg'; //Parcel 2
 import 'core-js/stable'; // polyfilling everything else
 import 'regenerator-runtime'; // polyfilling asyn await forold browsers
-console.log(icons);
 
 const recipeContainer = document.querySelector('.recipe');
 
@@ -32,11 +31,14 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
     // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc13'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -159,4 +161,8 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+
+// The same
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
