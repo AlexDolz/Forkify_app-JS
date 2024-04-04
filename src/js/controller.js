34f37e48a6fd7bf8830ee1ed6124928847f37abf
AlexDolz <aleksejdolznikov3@gmail.com>
2024-04-04 14:16:02 +0200

    implementing search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,13 +4,15 @@ import recipeView from './views/recipeView.js';
 import 'core-js/stable'; // polyfilling everything else
 import 'regenerator-runtime'; // polyfilling asyn await for old browsers
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
-const recipeContainer = document.querySelector('.recipe');
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -27,13 +29,14 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
     // 2) Load search results
     await model.loadSearchResults(query);
     // 3) Render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
