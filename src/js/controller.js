71bcfed0df1dc74a6c3d88839df56735abec1732
AlexDolz <aleksejdolznikov3@gmail.com>
2024-04-09 14:54:29 +0200

    implementing pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,9 +6,9 @@ import 'regenerator-runtime'; // polyfilling asyn await for old browsers
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
@@ -36,7 +36,8 @@ const controlSearchResults = async function () {
     // 2) Load search results
     await model.loadSearchResults(query);
     // 3) Render results
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
   } catch (err) {
     console.log(err);
   }
