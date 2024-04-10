96940b681363203e831949836256b6f5327762dd
AlexDolz <aleksejdolznikov3@gmail.com>
2024-04-10 13:21:09 +0200

    refactor dynamically generating buttons

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -1,6 +1,79 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg'; //Parcel 2
 
+// class PaginationView extends View {
+//   _parentElement = document.querySelector('.pagination');
+
+//   addHandlerClick(handler) {
+//     this._parentElement.addEventListener('click', function (e) {
+//       const btn = e.target.closest('.btn--inline');
+//       if (!btn) return;
+
+//       const goToPage = +btn.dataset.goto;
+//       handler(goToPage);
+//     });
+//   }
+
+//   _generateMarkup() {
+//     const curPage = this._data.page;
+//     const numPages = Math.ceil(
+//       this._data.results.length / this._data.resultsPerPage
+//     );
+//     // Page 1, and there are other pages
+//     if (curPage === 1 && numPages > 1) {
+//       return `
+//       <button data-goto="${
+//         curPage + 1
+//       }" class="btn--inline pagination__btn--next">
+//             <span>Page ${curPage + 1}</span>
+//             <svg class="search__icon">
+//               <use href="${icons}#icon-arrow-right"></use>
+//             </svg>
+//           </button>
+//       `;
+//     }
+
+//     // Last page
+//     if (curPage === numPages && numPages > 1) {
+//       return `
+//       <button data-goto="${
+//         curPage - 1
+//       }" class="btn--inline pagination__btn--prev">
+//             <svg class="search__icon">
+//               <use href="${icons}#icon-arrow-left"></use>
+//             </svg>
+//             <span>Page ${curPage - 1}</span>
+//           </button>
+//       `;
+//     }
+//     // Other page
+//     if (curPage < numPages) {
+//       return `
+//       <button data-goto="${
+//         curPage - 1
+//       }" class="btn--inline pagination__btn--prev">
+//             <svg class="search__icon">
+//               <use href="${icons}#icon-arrow-left"></use>
+//             </svg>
+//             <span>Page ${curPage - 1}</span>
+//           </button>
+//           <button data-goto="${
+//             curPage + 1
+//           }" class="btn--inline pagination__btn--next">
+//             <span>Page ${curPage + 1}</span>
+//             <svg class="search__icon">
+//               <use href="${icons}#icon-arrow-right"></use>
+//             </svg>
+//           </button>
+//       `;
+//     }
+//     // Page 1, and there are NO pages
+//     return '';
+//   }
+// }
+
+// export default new PaginationView();
+
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
@@ -14,59 +87,50 @@ class PaginationView extends View {
     });
   }
 
+  _generateMarkupButton(direction, page) {
+    return `
+      <button data-goto="${page}" class="btn--inline pagination__btn--${direction}">
+        ${
+          direction === 'prev'
+            ? `
+              <svg class="search__icon">
+                <use href="${icons}#icon-arrow-left"></use>
+              </svg>
+              <span>Page ${page}</span>`
+            : `
+              <span>Page ${page}</span>
+              <svg class="search__icon">
+                <use href="${icons}#icon-arrow-right"></use>
+              </svg>`
+        }
+      </button>
+    `;
+  }
+
   _generateMarkup() {
     const curPage = this._data.page;
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
+
     // Page 1, and there are other pages
     if (curPage === 1 && numPages > 1) {
-      return `
-      <button data-goto="${
-        curPage + 1
-      }" class="btn--inline pagination__btn--next">
-            <span>Page ${curPage + 1}</span>
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-right"></use>
-            </svg>
-          </button>
-      `;
+      return this._generateMarkupButton('next', curPage + 1);
     }
 
     // Last page
     if (curPage === numPages && numPages > 1) {
-      return `
-      <button data-goto="${
-        curPage - 1
-      }" class="btn--inline pagination__btn--prev">
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-left"></use>
-            </svg>
-            <span>Page ${curPage - 1}</span>
-          </button>
-      `;
+      return this._generateMarkupButton('prev', curPage - 1);
     }
+
     // Other page
     if (curPage < numPages) {
-      return `
-      <button data-goto="${
-        curPage - 1
-      }" class="btn--inline pagination__btn--prev">
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-left"></use>
-            </svg>
-            <span>Page ${curPage - 1}</span>
-          </button>
-          <button data-goto="${
-            curPage + 1
-          }" class="btn--inline pagination__btn--next">
-            <span>Page ${curPage + 1}</span>
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-right"></use>
-            </svg>
-          </button>
-      `;
+      return (
+        this._generateMarkupButton('prev', curPage - 1) +
+        this._generateMarkupButton('next', curPage + 1)
+      );
     }
+
     // Page 1, and there are NO pages
     return '';
   }
